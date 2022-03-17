70f6f3e7a582df8a8af7d974e69a15a4ff4fbd10
chilam <chilam.Lee@outlook.com>
2022-03-17 16:59:17 +0800

    :bug: fix bug

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,10 @@
 /*
  * @Description  :
  * @Author       : ch1lam
- * @Date         : 2022-03-08 15:01:14
- * @LastEditTime : 2022-03-08 21:49:16
+ * @Date         : 2022-03-17 16:07:39
+ * @LastEditTime : 2022-03-17 16:16:24
  * @LastEditors  : chilam
- * @FilePath     : \todo-app\src\routes\index.ts
+ * @FilePath     : \todo-app\server\src\routes\index.ts
  */
 import { Router } from "express";
 import {
@@ -13,11 +13,10 @@ import {
   updateTodo,
   deleteTodo,
 } from "../controllers/todos";
-
 const router: Router = Router();
 router.get("/todos", getTodos);
 router.post("/add-todo", addTodo);
 router.put("/edit-todo/:id", updateTodo);
-router.delete("/deleted-todo/:id", deleteTodo);
+router.delete("/delete-todo/:id", deleteTodo);
 
 export default router;
