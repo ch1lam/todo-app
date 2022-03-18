04e519f9c9871ef3d3a79967181188f55d6d6c41
chilam <chilam.Lee@outlook.com>
2022-03-18 21:20:44 +0800

    :sparkles: feat 前端页面

--- client/src/components/TodoItem.tsx
+++ client/src/components/TodoItem.tsx
@@ -1,8 +1,42 @@
 /*
- * @Description  : 
+ * @Description  :
  * @Author       : ch1lam
  * @Date         : 2022-03-08 22:31:56
- * @LastEditTime : 2022-03-08 22:31:56
+ * @LastEditTime : 2022-03-18 18:37:49
  * @LastEditors  : chilam
  * @FilePath     : \todo-app\client\src\components\TodoItem.tsx
  */
+import React from "react";
+
+type Props = TodoProps & {
+  updateTodo: (todo: ITodo) => void;
+  deleteTodo: (_id: string) => void;
+};
+
+const Todo: React.FC<Props> = ({ todo, updateTodo, deleteTodo }) => {
+  const checkTodo: string = todo.status ? `line-through` : "";
+  return (
+    <div className="Card">
+      <div className="Card--text">
+        <h1 className={checkTodo}>{todo.name}</h1>
+        <span className={checkTodo}>{todo.description}</span>
+      </div>
+      <div className="Card--button">
+        <button
+          onClick={() => updateTodo(todo)}
+          className={todo.status ? `hide-button` : "Card--button__done"}
+        >
+          Complete
+        </button>
+        <button
+          onClick={() => deleteTodo(todo._id)}
+          className="Card--button__delete"
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,15 @@
+/*
+ * @Description  : 
+ * @Author       : ch1lam
+ * @Date         : 2022-03-08 22:26:14
+ * @LastEditTime : 2022-03-18 18:03:56
+ * @LastEditors  : chilam
+ * @FilePath     : \todo-app\client\src\index.tsx
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +17,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
